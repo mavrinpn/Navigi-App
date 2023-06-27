18753c2fc2a5d0125ef29a30a1ec40b651e66074
domager73 <112970685+domager73@users.noreply.github.com>
2023-06-27 19:03:27 +0300

    add search screen

---
+++ lib/widgets/textField/outline_text_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/fonts.dart';
+
+class OutLineTextField extends StatelessWidget {
+  final double width;
+  final double height;
+  final String hintText;
+  final TextEditingController controller;
+  final TextInputType keyBoardType;
+  final bool obscureText;
+  final ValueChanged<String>? onChange;
+
+  const OutLineTextField({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+    this.width = 290,
+    this.height = 50,
+    this.obscureText = false,
+    this.keyBoardType = TextInputType.text,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: TextFormField(
+        onChanged: onChange,
+        style: AppTypography.font16black.copyWith(fontSize: 14),
+        decoration: const InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(13)),
+            borderSide: BorderSide(
+              color: Color(0xffE01748),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(13)),
+            borderSide: BorderSide(
+              color: Color(0xffE01748),
+            ),
+          ),
+        ),
+        controller: controller,
+      ),
+    );
+  }
+}
+
