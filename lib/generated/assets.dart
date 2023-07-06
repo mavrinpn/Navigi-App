5f618ce4b9eaae6d6b2a42efd058d8d31105c7e8
Jason1258t <ivandomogatskij@gmail.com>
2023-07-06 20:30:25 +0300

    msp full

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -6,14 +6,20 @@ class Assets {
   static const String assetsFacebook = 'Assets/facebook.png';
   static const String assetsGoogle = 'Assets/google.png';
   static const String assetsLogo = 'Assets/logo.png';
+  static const String assetsMapMarker = 'Assets/map_marker.png';
   static const String assetsMask = 'Assets/Mask.png';
+  static const String assetsRandomPeople = 'Assets/randomPeople.png';
   static const String assetsSplash = 'Assets/splash.png';
   static const String assetsSplashLogo = 'Assets/splash_logo.png';
   static const String assetsTestImage = 'Assets/test_image.png';
   static const String iconsAddImage = 'Assets/icons/add_image.svg';
+  static const String iconsCalendar = 'Assets/icons/calendar.svg';
+  static const String iconsDelete = 'Assets/icons/delete.svg';
   static const String iconsDeleteButton = 'Assets/icons/delete_button.svg';
+  static const String iconsDzd = 'Assets/icons/dzd.svg';
   static const String iconsEmail = 'Assets/icons/email.svg';
   static const String iconsError = 'Assets/icons/error.svg';
+  static const String iconsExit = 'Assets/icons/exit.svg';
   static const String iconsEye = 'Assets/icons/eye.svg';
   static const String iconsFacebook = 'Assets/icons/facebook.svg';
   static const String iconsFollow = 'Assets/icons/follow.svg';
@@ -24,7 +30,11 @@ class Assets {
   static const String iconsLike = 'Assets/icons/like.svg';
   static const String iconsLogo = 'Assets/icons/logo.svg';
   static const String iconsMenuDotsVertical = 'Assets/icons/menu-dots-vertical.svg';
+  static const String iconsOnlySearch = 'Assets/icons/only_search.svg';
   static const String iconsPhone = 'Assets/icons/phone.svg';
+  static const String iconsPoint = 'Assets/icons/point.svg';
   static const String iconsProfile = 'Assets/icons/profile.svg';
   static const String iconsSearch = 'Assets/icons/search.svg';
+  static const String iconsThreeDots = 'Assets/icons/three_dots.svg';
+
 }
