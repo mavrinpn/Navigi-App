3003f598a36ba73c49b0b2f218eb94689c937eee
pavel_mavrin <mavrinpn@mail.ru>
2024-06-01 10:10:26 +0400

    update internet connectivity

--- lib/feature/main/ui/main_page.dart
+++ lib/feature/main/ui/main_page.dart
@@ -6,7 +6,6 @@ import 'package:internet_connection_checker_plus/internet_connection_checker_plu
 import 'package:smart/bloc/app/app_cubit.dart';
 import 'package:smart/feature/home/ui/home_screen.dart';
 import 'package:smart/localization/app_localizations.dart';
-import 'package:smart/utils/constants.dart';
 import 'package:smart/widgets/snackBar/snack_bar.dart';
 import 'package:smart/widgets/splash.dart';
 
@@ -21,27 +20,17 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   bool hasConnection = true;
-  // final Connectivity _connectivity = Connectivity();
-  // late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
   late StreamSubscription<InternetStatus> _internetConnectionSubscription;
 
   @override
   void initState() {
     super.initState();
-    // initConnectivity();
-    // _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
 
-    final connection = InternetConnection.createInstance(
-      customCheckOptions: [
-        InternetCheckOption(uri: Uri.parse('$serviceProtocol$serviceDomain')),
-      ],
-    );
+    final connection = InternetConnection.createInstance();
     _internetConnectionSubscription = connection.onStatusChange.listen((InternetStatus status) {
       final localizations = AppLocalizations.of(context)!;
       switch (status) {
         case InternetStatus.connected:
-          // hasConnection = true;
-          // setState(() {});
           break;
         case InternetStatus.disconnected:
           CustomSnackBar.showSnackBarWithIcon(
@@ -49,8 +38,6 @@ class _MainPageState extends State<MainPage> {
             text: localizations.noConnection,
             iconData: Icons.wifi_off,
           );
-          // hasConnection = false;
-          // setState(() {});
           break;
       }
     });
@@ -58,31 +45,9 @@ class _MainPageState extends State<MainPage> {
 
   @override
   dispose() {
-    // _connectivitySubscription.cancel();
     _internetConnectionSubscription.cancel();
     super.dispose();
   }
-
-  // Future<void> initConnectivity() async {
-  //   late List<ConnectivityResult> result;
-  //   try {
-  //     result = await _connectivity.checkConnectivity();
-  //   } on PlatformException catch (e) {
-  //     debugPrint('Couldn\'t check connectivity status $e');
-  //     return;
-  //   }
-  //   if (!mounted) {
-  //     return Future.value(null);
-  //   }
-
-  //   return _updateConnectionStatus(result);
-  // }
-
-  // Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
-  //   setState(() {
-  //     // hasConnection = !result.contains(ConnectivityResult.none);
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -90,9 +55,6 @@ class _MainPageState extends State<MainPage> {
       resizeToAvoidBottomInset: false,
       body: BlocBuilder<AppCubit, AppState>(
         builder: (context, state) {
-          // if (!hasConnection) {
-          //   return const NoConnection();
-          // }
           if (state is AppAuthState || state is AppUnAuthState) {
             return const HomeScreen();
           } else {
